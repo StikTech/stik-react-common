50be3f8c86f9178cb51ee6b969dcebed3b77d391
nab138 <nab@nabdev.me>
2025-11-28 00:47:29 -0500

    include dist

---
+++ dist/components/GlassCard.d.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+type GlassCardProps = {
+    children: ReactNode;
+    className?: string;
+    style?: React.CSSProperties;
+};
+export declare const GlassCard: ({ children, className, style }: GlassCardProps) => import("react/jsx-runtime").JSX.Element;
+export {};
---
+++ dist/main.js
@@ -0,0 +1,10 @@
+import { jsx as o } from "react/jsx-runtime";
+import './assets/main.css';const c = {
+  "glass-card": "_glass-card_9ryz3_1"
+}, e = ({ children: s, className: a, style: r }) => {
+  const l = [c["glass-card"], a].filter(Boolean).join(" ");
+  return /* @__PURE__ */ o("div", { className: l, style: r, children: s });
+};
+export {
+  e as GlassCard
+};
